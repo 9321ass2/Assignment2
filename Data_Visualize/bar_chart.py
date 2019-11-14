ebce49c3f49df07e68c4a9875d08a89716caeaa6
Marcusmin <47769089+Marcusmin@users.noreply.github.com>
2019-11-15 02:42:35 +1100

    add bar_chart sample
    Q: same game in different platform ?

--- Data_Visualize/bar_chart.py
+++ Data_Visualize/bar_chart.py
@@ -6,10 +6,24 @@ import pandas as pd
 def read_csv(csv_file):
     return pd.read_csv(csv_file)
 
-def pre_process(dataframe, region='Global_Sales', genere='all', platform='all', s_or_r = 0):
+def pre_process(dataframe, region='Global_Sales', genere=None, platform=None, s_or_r = 0):
+    num_list = ['Critic_Score', 'User_Score', 'Year', 'Total_Shipped', 'Global_Sales', 'NA_Sales', 'PAL_Sales', 'JP_Sales', 'Other_Sales']
+    for each in num_list:
+        dataframe[each].fillna(0, inplace = True)
+    dataframe['Global_Sales'] += dataframe['Total_Shipped'] #merge Total_Shipped and Global_Sales into Global_Sales
+    useless_list = ['Total_Shipped'] #useless columns, modify by yourself
+    dataframe.drop(columns = useless_list, inplace = True)
     if s_or_r:
-        dataframe.sort_values(by = ['Global_Sales'], ascending  = False, inplace = True)
-        sale = dataframe.head(10)[['Name', 'Platform', region]]
+        dataframe.sort_values(by = [region], ascending  = False, inplace = True)
+        if genere:
+            dataframe = dataframe[dataframe['Genre'].str.contains(genere)]
+            sale = dataframe.head(10)[['Name', 'Platform', region]]
+        if platform:
+            dataframe = dataframe[dataframe['Platform'].str.contains(platform)]
+            sale = dataframe.head(10)[['Name', 'Genre', region]]
+        if genere and platform:
+            sale = dataframe.head(10)[['Name', region]]
+        sale = dataframe.head(10)[['Name', region]]
         return sale
     else:
         dataframe.sort_values(by = ['Critic_Score'], ascending  = False, inplace = True)
@@ -17,11 +31,10 @@ def pre_process(dataframe, region='Global_Sales', genere='all', platform='all', 
         print(sale)
 
 def display_chart(dataframe):
-    dataframe.sort_values(by = ['Global_Sales'], ascending  = True, inplace = True)
-    x = dataframe['Global_Sales']
-    y = dataframe['Name']
-    print(dataframe)
-    plt.barh(dataframe['Name'], dataframe['Global_Sales'], height = 0.5)
+    dataframe.sort_values(by = dataframe.columns[-1], ascending  = True, inplace = True)
+    x = dataframe[dataframe.columns[-1]]
+    y = dataframe[dataframe.columns[0]]
+    plt.barh(y,x, height = 0.5)
     plt.title('Top Sales Games for All Platforms')
     plt.yticks(fontsize=5)
     plt.show()
@@ -32,6 +45,9 @@ if __name__ == '__main__':
     csv_file = r'\DataSet\vgsales-12-4-2019-short.csv'
     csv_file = path + csv_file
     df = read_csv(csv_file)
-    processed_df = pre_process(df, s_or_r = 1)
+    #processed_df = pre_process(df, s_or_r = 1, genere = 'Action')
+    #processed_df = pre_process(df, s_or_r = 1, platform = 'PC')
+    processed_df = pre_process(df, s_or_r = 1, genere = 'Action', platform = 'PC')
+    #processed_df = pre_process(df, s_or_r = 1)
     display_chart(processed_df)
 
