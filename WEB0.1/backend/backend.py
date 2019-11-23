a244e5ece17e6c27ec8901407c247d26bee63cfa
chen <jubi33717f@gmail.com>
2019-11-23 13:25:24 +1100

    web0.2

--- WEB0.1/backend/backend.py
+++ WEB0.1/backend/backend.py
@@ -4,19 +4,6 @@ from flask_cors import CORS
 
 
 
-# def run(host, port):
-#     if port is not None:
-#         run1(host, port)
-#     else:
-#         for port in range(5000, 5100):
-#             try:
-#                 DataToday()
-#                 app.run(debug=True, host=host, port=port)
-#                 CORS(app)
-#                 break
-#             except OSError as e:
-#                 if 'Address in use' in str(e):
-#                     continue
 
 if __name__ == '__main__':
     import namespaces.authentication
@@ -26,9 +13,7 @@ if __name__ == '__main__':
     import namespaces.country
     import namespaces.recom
     import namespaces.usage
-    # host='127.0.0.1'
-    # port=None
-    # run(host, port)
+
     DataToday()
     app.run(debug=True)
     CORS(app)
